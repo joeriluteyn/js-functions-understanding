5da42b4657e117deb24f0201507e0207f0ba7fdb
joeri luteyn <joeriluteyn@gmail.com>
2023-04-11 11:58:49 +0200

    q15 done

---
+++ q15.js
@@ -0,0 +1,13 @@
+function myFunction(num1) {
+    if(true) {
+      return -10
+    }
+  
+    return num1 * 10
+  }
+  
+  const result = myFunction(5)
+
+  // it will return -10  because statement is true 
+
+  console.log(result);
