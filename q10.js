b13f770c3225c1d477a71899ab18b7b618963116
joeri luteyn <joeriluteyn@gmail.com>
2023-04-11 11:24:51 +0200

    q10 done

---
+++ q10.js
@@ -0,0 +1,12 @@
+function myFunction(num1, num2, num3) {
+    console.log(num3)
+  }
+  
+  const num1 = 10
+  const num2 = 1
+  const num3 = 20
+  
+  myFunction(num3, num1, 100)
+
+
+  // prints out 100 because that is the parameter called which is specified as 100
