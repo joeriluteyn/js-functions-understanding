57cef4fa2a6d82644b09f2ed4eeb3c62b6142a33
joeri luteyn <joeriluteyn@gmail.com>
2023-04-11 11:20:27 +0200

    q8 done

---
+++ q8.js
@@ -0,0 +1,13 @@
+function myFunction(num1, num2) {
+    return num1 + num2
+  }
+  
+  const num1 = 10
+  const num2 = 1
+  const num3 = 4
+  
+  const result = myFunction(num3, num1)
+
+  // the result will be 14 because num1 and num3 are the arguments 
+
+  console.log(result)
