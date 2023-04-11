50d149e557ce712fdebee1de6fe0326640bd7999
joeri luteyn <joeriluteyn@gmail.com>
2023-04-11 11:16:15 +0200

    q6 done

---
+++ q6.js
@@ -0,0 +1,11 @@
+function myFunction(num, num1) {
+    console.log(num1)
+  }
+  
+  let num = 10
+  let num1 = 2
+  
+  myFunction(num1, num)
+
+// 10 will be logged because the parameters are already defined by place
+
