c1238a46990f627b726fa5eabb550b9a07bcd605
joeri luteyn <joeriluteyn@gmail.com>
2023-04-11 11:13:55 +0200

    q5 done

---
+++ q5.js
@@ -0,0 +1,12 @@
+function myFunction(num, num1) {
+    console.log(num1)
+  }
+  
+  let num = 10
+  let num1 = 2
+  
+  myFunction(num)
+
+  // the log will be undifined because num1 is not specified
+
+  
