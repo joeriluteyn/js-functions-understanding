2fd0b07c7729ff47dc6373532a8c0a01eafa1215
joeri luteyn <joeriluteyn@gmail.com>
2023-04-11 11:22:49 +0200

    q9 done

---
+++ q9.js
@@ -0,0 +1,13 @@
+function myFunction(num1, num2) {
+    console.log(num3)
+  }
+  
+  const num1 = 10
+  const num2 = 1
+  const num3 = 20
+  
+  myFunction(num3, num1)
+
+  // it will log 20 because it can acces the variable num 3 outside of the function
+
+  
