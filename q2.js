a253825e31fa4ce682d4ea0833336c3c80099f96
joeri luteyn <joeriluteyn@gmail.com>
2023-04-11 11:04:41 +0200

    q2 done

---
+++ q2.js
@@ -0,0 +1,11 @@
+function myFunction(num1, num2) {
+    num1+num2
+  }
+  
+  const result = myFunction(5,5)
+
+  // expected result = function will run but there is nothing to return 
+
+  console.log(result)
+
+  // the reason it is undifined is beacuse we log result but there is nothing in the function so we get undifined 
