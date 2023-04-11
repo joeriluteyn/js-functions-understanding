ece8c1475fc23d4244fe6552e0dc14987f97e458
joeri luteyn <joeriluteyn@gmail.com>
2023-04-11 11:26:44 +0200

    q11 done

---
+++ q11.js
@@ -0,0 +1,11 @@
+function myFunction(num1, num2, num3) {
+    return num1 + num2 + num3
+  }
+  
+  const num1 = 10
+  const num2 = 1
+  const num3 = 20
+  
+  const result = myFunction(1, 1, 1)
+
+  // the result will be 3 because this are the arguments given
